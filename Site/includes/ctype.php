0eee8ea623a7188a1a1a2ce2acfbd6e18bf5ab92
Hyabusa1990 <gero@gras-it.de>
2017-04-15 22:57:46 +0200

    Initial Fill Classes

--- Site/includes/ctype.php
+++ Site/includes/ctype.php
@@ -1,8 +1,47 @@
 <?php
-if (!defined('WERDICHLEGALGERUFEN')) {
-    echo 'You cannot access this file directly!';
-    die;
+if (!defined('WERDICHLEGALGERUFEN'))
+{
+	echo 'You cannot access this file directly!';
+	die;
 }
 
-	
+class CType
+{
+	private $ID = 0;
+	private $title;
+	private $icon;
+
+	public function __constructor($pID, $pTitle, $pIcon)
+	{
+		$this->$ID = $pID;
+		$this->set_Title($pTitle);
+		$this->set_Icon($pIcon);
+	}
+
+	public function get_ID()
+	{
+		return $this->$ID;
+	}
+
+	public function get_Title()
+	{
+		return $this->$title;
+	}
+
+	public function set_Title($pTitle)
+	{
+		$this->$title = $pTitle;
+	}
+
+	public function get_Icon()
+	{
+		return $this->$icon;
+	}
+
+	public function set_Icon($pIcon)
+	{
+		$this->$icon = $pIcon;
+	}
+
+}
 ?>
--- Site/includes/database.php
+++ Site/includes/database.php
@@ -1,10 +1,27 @@
 <?php
-if (!defined('WERDICHLEGALGERUFEN')) {
-  echo 'You cannot access this file directly!';
-  die;
+if (!defined('WERDICHLEGALGERUFEN'))
+{
+	echo 'You cannot access this file directly!';
+	die;
 }
+require_once ('settings.php');
 
+class Database
+{
 
+	function login()
+	{
+		$con = mysqli_connect(Settings::get_dbServer(), Settings::get_dbUser(), Settings::
+		get_dbPw());
+		mysqli_select_db($con, Settings::get_dbName());
+		mysqli_set_charset($con, 'utf8');
+		return $con;
+	}
 
+	function closeDB($con)
+	{
+		mysqli_close($con);
+	}
 
+}
 ?>
